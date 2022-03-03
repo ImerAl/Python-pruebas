505711dbffbb2a1e9bf1c6d937e17cee809030bc
ImerAl <imeralfred@gmail.com>
2022-03-03 15:17:00 -0600

    Basic & data structures

---
+++ Data Structure/DS_Tuplev2.py
@@ -0,0 +1,14 @@
+#tuple() Para crear cualquier wea a tupla
+
+#Tupla vacia
+tup1 = tuple()
+
+#Lista a tupla
+tup2 = tuple([1,2,3])
+
+#String a tupla
+tup3 = tuple('Unravel')
+
+#Dictionary a tupla
+tup4 = tuple({1:'one', 2:'two'}) #Solo toma las claves
+

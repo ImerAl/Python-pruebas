d5edd3c23bb5f23a2f3aab525e5e9c931103fecd
ImerAl <imeralfred@gmail.com>
2022-03-07 18:09:41 -0600

    OOP

---
+++ Conditional loops/OOP.py
@@ -0,0 +1,13 @@
+class MyClass():
+    def method1 (self):
+        print("Mamahuevo")
+    def method2(self, someString):
+        print("Soware " + someString)
+
+def main():
+    c = MyClass()
+    c.method1()
+    c.method2("Qliao")
+
+if __name__ == "__main__":
+    main()

61144881ff475e4f7dcc7a0d5696a3843221f699
ImerAl <imeralfred@gmail.com>
2022-03-08 19:00:27 -0600

    Strings

---
+++ Strings/Strp.py
@@ -0,0 +1,7 @@
+#Strip removes characters, by default removes blank spaces
+#STRIP YA NO FUNCA JJAJAJAJAJAJAJAJAJAJAJAJJ ahora se usa replace
+
+var = "Wachin qliao"
+
+s = var.replace('W','')
+print(s)
